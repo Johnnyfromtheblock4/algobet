3e5bdcf87bb6471836fa4aa0fbe41e86fff75935
Johnnyfromtheblock4 <simo.visco4@gmail.com>
2025-11-01 23:33:34 +0100

    [UPD] DeafultLayout

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
 
 const Homepage = () => {
   return (
     <div>
-      
+      <div className="container my-5">
+        <div className="row">
+          <div className="col-12 text-center">
+            <h1 className="title">
+              Algo<strong className="title-2">bet</strong>
+            </h1>
+            <h5 className="subtitle">The new way of betting</h5>
+          </div>
+          <div className="col-12 col-md-6">
+            <img src="../algobet-logo.png" className="img-fluid" />
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Homepage
+export default Homepage;
